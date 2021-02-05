dc417d162bf4924754ab603a9698802f67106c39
Marcosjedai <alan325_batera@hotmail.com>
2021-02-05 14:00:07 -0300

    Chess layer and priece the board

--- src/application/Program.java
+++ src/application/Program.java
@@ -4,12 +4,17 @@ package application;
 
 import boardgame.Board;
 import boardgame.Position;
+import chess.ChessMatch;
 
 public class Program {
 
 	public static void main(String[] args) {
+		ChessMatch chessmatch = new ChessMatch();
 		
-		Board board = new Board(8,8 );
+		UI.printBoard(chessmatch.getPiece());
+	    
+		
 	}
+	
 
 }
